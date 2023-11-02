2e42bf4351b06dd11e8c64e1cf7ac2ce9387dd9a
monsefblzzz <monsefblouz@gmail.com>
2023-11-02 10:03:17 +0100

    .

---
+++ app/src/main/java/com/example/pokemonsef/pokeAPI.java
@@ -0,0 +1,10 @@
+package com.example.pokemonsef;
+
+public class pokeAPI {
+
+    private final String BASE_URL = "https://pokeapi.co/api/v2/";
+    private final String API_KEY = "<api-key>";
+
+    
+
+}
